d2c7f85c19fb1d3b5d23282b73b8bfaba0303aa1
tranvanquyetn <48422559+tranvanquyetn@users.noreply.github.com>
2019-05-22 22:52:04 +0700

    Add files via upload

---
+++ thuchanh6_2.py
@@ -0,0 +1,21 @@
+
+class Hinhchunhat (object):
+    def __init__(self, dai, rong):
+        self.dai = dai
+        self.rong = rong
+
+
+    def area(self):
+      return self.dai * self.rong
+
+aHinhchunhat =  Hinhchunhat(10, 2)
+print (aHinhchunhat.area())
+
+
+
+
+
+
+
+
+
